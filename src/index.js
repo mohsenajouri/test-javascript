a7be3b35605fb6294078c2245d9faf1057132e5d
mohsenajouri <155250668+mohsenajouri@users.noreply.github.com>
2025-11-28 23:07:57 +0330

    fifth practice java untill lesson 6

---
+++ src/index.js
@@ -0,0 +1,85 @@
+let myText = " lorem jknkjm kjmnlk;jm;l oljkmol'km, ojkmpo";
+const PI = 3.14;
+let myFirstName = "vdnbvjhdb jdvbjdnb djvbndaj";
+let firstName = "Mohsen",
+  lastName = "Ajouri",
+  age = 45,
+  age1 = "19";
+let testOne;
+let education = 12e5;
+let myText1 = false;
+let myTest2 = null;
+var test3 = "dgdjn";
+let test4 = "fgeufujdhn";
+test4 = 12;
+/*const test5 = 345;
+test5 = "dssf";*/
+const userName = "fsjfnis";
+let lastLogin = "17:50";
+lastLogin = "15:25";
+lastLogin = "17:25";
+const firstNum = 18;
+const secondNum = 30;
+const result = firstNum * secondNum;
+console.log(result);
+console.log( 10 % 4 );
+
+let x = 5;
+console.log( x++ ); //x = x + 1  x++
+console.log( x );
+console.log( ++x );
+console.log( --x ); // x = x-1
+let x1 = 10;
+let y;
+x = y;
+//y = x;
+console.log( x , y);
+
+let y1 = 5;
+x1 = x1 +y1; 
+x1 +=y1
+console.log( x1 );
+let x2 = 5;
+x2 *=y1;
+console.log( x2 );
+let x3 = 25;
+x3 /=y1;
+console.log( x3 );
+
+// x4 % y1 x4 = x4 % y1
+let x4 =25;
+x4 %= y1
+console.log( x4 );
+
+// x5 = x5 ** y1    x5 **=y1
+let x5 = 3;
+x5 **= y1;
+console.log( x5 );
+console.log(firstName);
+/*console.log(lastName);
+console.log(age);
+console.log( age1 );
+console.log( testOne );
+console.log( education );
+console.log( myText1 );
+console.log( typeof myText1 );*/
+console.log(typeof myTest2);
+console.log(typeof education);
+console.log(myTest2);
+console.log(lastLogin);
+
+let x6 = 5;
+let y6 = 5;
+let z6 = "5";
+console.log( x6 + y6 );
+console.log( x6 + z6 );
+console.log(typeof x6 + z6  );
+let result6 = x6 + z6;
+console.log( typeof result6 );
+let result7 = y6 - z6 ;
+console.log( result7 );
+console.log( typeof result7 );
+let result8 = y6 * z6;
+console.log( result8 );
+console.log( typeof result8 );
+
